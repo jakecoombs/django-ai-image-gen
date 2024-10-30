58be81f97de29d23d5eb73ab36ea923c3771add8
jakecoombs <57802613+jakecoombs@users.noreply.github.com>
2024-10-30 17:01:21 +0000

    Create form

--- gen/urls.py
+++ gen/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "gen"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.generate_image, name="generate_image"),
 ]
--- gen/views.py
+++ gen/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from gen.forms import ImageGeneratorForm
 
 
 def index(request):
     return HttpResponse("Create an image")
+
+
+def generate_image(request):
+    if request.method == "POST":
+        form = ImageGeneratorForm(request.POST)
+        if form.is_valid():
+            # Generate Image
+            print("done")
+
+    else:
+        form = ImageGeneratorForm()
+
+    return render(request, "gen/generate_image.html", {"form": form})
